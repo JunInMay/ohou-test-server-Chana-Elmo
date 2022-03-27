960cd161b1d42cf8a57948d9fbb271f6158b6de7
Joon <shuangjutou@naver.com>
2022-03-27 19:57:02 +0900

    feat : 노하우 리스트 조회 API 구현

--- ozip/src/main/java/shop/ozip/dev/src/feed/model/GetFeedsKnowhowFeedsListRes.java
+++ ozip/src/main/java/shop/ozip/dev/src/feed/model/GetFeedsKnowhowFeedsListRes.java
@@ -4,14 +4,13 @@ package shop.ozip.dev.src.feed.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import shop.ozip.dev.src.keyword.model.Keyword;
 
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class MediaWithKeyword {
-    private Media media;
-    private List<Keyword> keywords;
+public class GetFeedsKnowhowFeedsListRes {
+    private Integer feedCount;
+    private List<GetFeedsKnowhowFeedsListResFeed> knowhowFeeds;
 }
---
+++ ozip/src/main/java/shop/ozip/dev/src/feed/model/GetFeedsMediaFeedResMedia.java
@@ -0,0 +1,24 @@
+
+package shop.ozip.dev.src.feed.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetFeedsMediaFeedResMedia {
+    private Long id;
+    private Long feedId;
+    private String thumbnailUrl;
+    private Integer time;
+    private String description;
+    private String url;
+    private Long mediaSpaceTypeId;
+    private int isPhoto;
+    private int isVideo;
+    private String createdAt;
+    private String updatedAt;
+    private Integer isBookmarked;
+}
