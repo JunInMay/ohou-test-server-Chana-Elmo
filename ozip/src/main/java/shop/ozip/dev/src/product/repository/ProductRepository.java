6712c7a9a725364f1f97c34d13db80d0efdd2890
Joon <shuangjutou@naver.com>
2022-03-29 15:18:28 +0900

    test

--- ozip/src/main/java/shop/ozip/dev/src/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.ozip.dev.src.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.ozip.dev.src.product.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- ozip/src/main/java/shop/ozip/dev/src/product/repository/TodayDealRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.ozip.dev.src.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.ozip.dev.src.product.entity.TodayDeal;
-
-public interface TodayDealRepository extends JpaRepository<TodayDeal, Long> {
-}
