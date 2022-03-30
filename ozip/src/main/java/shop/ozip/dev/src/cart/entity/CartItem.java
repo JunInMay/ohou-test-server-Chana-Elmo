026963b898025fa0a14c8c5a70ef5e43332eaf16
kmo6bf <kmo6bf@gmail.com>
2022-03-30 15:31:46 +0900

    feat : 장바구니 Entity 생성

---
+++ ozip/src/main/java/shop/ozip/dev/src/cart/entity/CartItem.java
@@ -0,0 +1,40 @@
+package shop.ozip.dev.src.cart.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.ozip.dev.src.global.repository.BaseTimeEntity;
+import shop.ozip.dev.src.product.entity.Product;
+import shop.ozip.dev.src.product.option.entity.MachineOption;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+public class CartItem extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "productId", referencedColumnName = "id", nullable = false)
+    private Product productId;
+
+    @ManyToOne
+    @JoinColumn(name = "optionId", referencedColumnName = "id", nullable = false)
+    private MachineOption optionId;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Column(nullable = false)
+    private int totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "cartId", referencedColumnName = "id", nullable = false)
+    private Cart cartId;
+}
